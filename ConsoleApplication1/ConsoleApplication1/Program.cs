4bb2a83c30630750e5dc63418d3d69d6a3c22162
tantipina <tatyana.buchneva@mail.ru>
2016-03-02 22:13:39 +0300

    change projects

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,53 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            int[,] myMassive = new int[2, 2];
+            string rawStr = Console.ReadLine();
+            while (rawStr == null) // проверка массива на пустоту
+            {
+                Console.WriteLine("Введите не пустой массив");
+                rawStr = Console.ReadLine();
+            }
+
+            var rawArray = rawStr.Split(' ');
+
+            // проверка на соответствие массива требуемой размерности
+            while (rawArray.Length != 4)
+            {
+                Console.WriteLine("Массив должен содержать 2*2 элемента. Введите массив заново");
+                rawStr = Console.ReadLine();
+                rawArray = rawStr.Split(' ');
+            }
+            try
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    for (int j = 0; j < 2; j++)
+                    {
+                        int next;
+                        if (int.TryParse(rawArray[i * 2 + j], out next))
+                            myMassive[i, j] = next;
+                        else
+                            throw new Exception("Массив содержит не числовой элемент");//если очередной элемент не число
+                    }
+                }
+                Console.WriteLine("Array is");
+                for (int i = 0; i < 2; i++)
+                {
+                    for (int j = 0; j < 2; j++)
+                    {
+                        Console.Write(myMassive[i, j]);
+                        Console.Write(' ');
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
